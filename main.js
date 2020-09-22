62b65b1b308994125c4d47343ad4b43759ec6db2
Juan Antonio <juanchis114@hotmail.com>
2020-09-23 01:11:14 +0200

    sexto commit

--- main.js
+++ main.js
@@ -1,5 +1,294 @@
-const carro = document.getElementById('carro');
+// cogemos el elemento clase de cada una de las frutas con dragstart
 
+const miFruta0 = document.getElementById('miFruta0');
+
+miFruta0.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta0.className);
+})
+
+const miFruta1 = document.getElementById('miFruta1');
+
+miFruta1.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta1.className);
+})
+
+const miFruta2 = document.getElementById('miFruta2');
+
+miFruta2.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta2.className);
+})
+
+const miFruta3 = document.getElementById('miFruta3');
+
+miFruta3.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta3.className);
+})
+
+const miFruta4 = document.getElementById('miFruta4');
+
+miFruta4.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta4.className);
+})
+
+const miFruta5 = document.getElementById('miFruta5');
+
+miFruta5.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta5.className);
+})
+
+const miFruta6 = document.getElementById('miFruta6');
+
+miFruta6.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta6.className);
+})
+
+const miFruta7 = document.getElementById('miFruta7');
+
+miFruta7.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta7.className);
+})
+
+const miFruta8 = document.getElementById('miFruta8');
+
+miFruta8.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta8.className);
+})
+
+const miFruta9 = document.getElementById('miFruta9');
+
+miFruta9.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta9.className);
+})
+
+const miFruta10 = document.getElementById('miFruta10');
+
+miFruta10.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta10.className);
+})
+
+const miFruta11 = document.getElementById('miFruta11');
+
+miFruta11.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta11.className);
+})
+
+const miFruta12 = document.getElementById('miFruta12');
+
+miFruta12.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta12.className);
+})
+
+const miFruta13 = document.getElementById('miFruta13');
+
+miFruta13.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta13.className);
+})
+
+const miFruta14 = document.getElementById('miFruta14');
+
+miFruta14.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta14.className);
+})
+
+const miFruta15 = document.getElementById('miFruta15');
+
+miFruta15.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta15.className);
+})
+
+const miFruta16 = document.getElementById('miFruta16');
+
+miFruta16.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta16.className);
+})
+
+const miFruta17 = document.getElementById('miFruta17');
+
+miFruta17.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta17.className);
+})
+
+const miFruta18 = document.getElementById('miFruta18');
+
+miFruta18.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta18.className);
+})
+
+const miFruta19 = document.getElementById('miFruta19');
+
+miFruta19.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta19.className);
+})
+
+const miFruta20 = document.getElementById('miFruta20');
+
+miFruta20.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta20.className);
+})
+
+const miFruta21 = document.getElementById('miFruta21');
+
+miFruta21.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta21.className);
+})
+
+const miFruta22 = document.getElementById('miFruta22');
+
+miFruta22.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta22.className);
+})
+
+const miFruta23 = document.getElementById('miFruta23');
+
+miFruta23.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta23.className);
+})
+
+const miFruta24 = document.getElementById('miFruta24');
+
+miFruta24.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta24.className);
+})
+
+const miFruta25 = document.getElementById('miFruta25');
+
+miFruta25.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta25.className);
+})
+
+const miFruta26 = document.getElementById('miFruta26');
+
+miFruta26.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta26.className);
+})
+
+const miFruta27 = document.getElementById('miFruta27');
+
+miFruta27.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta27.className);
+})
+
+const miFruta28 = document.getElementById('miFruta28');
+
+miFruta28.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta28.className);
+})
+
+const miFruta29 = document.getElementById('miFruta29');
+
+miFruta29.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta29.className);
+})
+
+const miFruta30 = document.getElementById('miFruta30');
+
+miFruta30.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta30.className);
+})
+
+const miFruta31 = document.getElementById('miFruta31');
+
+miFruta31.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta31.className);
+})
+
+const miFruta32 = document.getElementById('miFruta32');
+
+miFruta32.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta32.className);
+})
+
+const miFruta33 = document.getElementById('miFruta33');
+
+miFruta33.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta33.className);
+})
+
+const miFruta34 = document.getElementById('miFruta34');
+
+miFruta34.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta34.className);
+})
+
+const miFruta35 = document.getElementById('miFruta35');
+
+miFruta35.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta35.className);
+})
+
+const miFruta36 = document.getElementById('miFruta36');
+
+miFruta36.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta36.className);
+})
+
+const miFruta37 = document.getElementById('miFruta37');
+
+miFruta37.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta37.className);
+})
+
+const miFruta38 = document.getElementById('miFruta38');
+
+miFruta38.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta38.className);
+})
+
+const miFruta39 = document.getElementById('miFruta39');
+
+miFruta39.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta39.className);
+})
+
+const miFruta40 = document.getElementById('miFruta40');
+
+miFruta40.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta40.className);
+})
+
+const miFruta41 = document.getElementById('miFruta41');
+
+miFruta41.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta41.className);
+})
+
+const miFruta42 = document.getElementById('miFruta42');
+
+miFruta42.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta42.className);
+})
+
+const miFruta43 = document.getElementById('miFruta43');
+
+miFruta43.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta43.className);
+})
+
+const miFruta44 = document.getElementById('miFruta44');
+
+miFruta44.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta44.className);
+})
+
+const miFruta45 = document.getElementById('miFruta45');
+
+miFruta45.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta45.className);
+})
+
+const miFruta46 = document.getElementById('miFruta46');
+
+miFruta46.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta46.className);
+})
+
+const miFruta47 = document.getElementById('miFruta47');
+
+miFruta47.addEventListener("dragstart", e=>{
+    e.dataTransfer.setData("text/plain",miFruta47.className);
+})
+
+//inicializamos las frutas y damos valor false si no hay ninguna en el carro
 let precioPera = 0;
 let hayPera=false;
 
@@ -24,14 +313,20 @@ let hayCebolla=false;
 let precioPlatano = 0;
 let hayPlatano=false;
 
+// usamos carro como elemento donde hacer drop y recoger los datos de las imagenes
+const carro = document.getElementById('carro');
+ 
 carro.addEventListener('dragover', e=>{
     e.preventDefault();
 },false);
 
 carro.addEventListener('drop', e=>{
     e.preventDefault();
-    let data = e.dataTransfer.getData('text');
-    if(data=='https://github.com/juanakan/Supermercado/blob/master/images/pera.png'){
+    const frutaClass = e.dataTransfer.getData("text/plain");
+
+    //creamos los if donde preguntamos que fruta es y creamos un parrafo con la fruta y el precio
+
+    if(frutaClass=='pera'){
         if(!hayPera){
             precioPera=2;
             let parrafo = document.createElement('p');
@@ -47,10 +342,9 @@ carro.addEventListener('drop', e=>{
             document.getElementById('costoPera').innerHTML=`Peras: ${precioPera} €`;
             hayPera=true;
         }
-
         
     }
-    if(data=='https://github.com/juanakan/Supermercado/blob/master/images/naranja.png'){
+    if(frutaClass=='naranja'){
         if(!hayNaranja){
             precioNaranja=2.5;
             let parrafo = document.createElement('p');
@@ -67,9 +361,8 @@ carro.addEventListener('drop', e=>{
                 hayNaranja=true;
         }
 
-        
     }
-    if(data=='https://github.com/juanakan/Supermercado/blob/master/images/zanahoria.png'){
+    if(frutaClass=='zanahoria'){
         if(!hayZanahoria){
             precioZanahoria=1.2;
             let parrafo = document.createElement('p');
@@ -82,13 +375,13 @@ carro.addEventListener('drop', e=>{
             }else{
                 precioZanahoria +=1.2;
 
-            document.getElementById('costoZanahoria').innerHTML=`Zanahorias: ${precioZanahoria}€`;
-            hayZanahoria=true;
-        }
+                document.getElementById('costoZanahoria').innerHTML=`Zanahorias: ${precioZanahoria}€`;
+                hayZanahoria=true;
+                }
 
         
     }
-    if(data=='https://github.com/juanakan/Supermercado/blob/master/images/sandia.png'){
+    if(frutaClass=='sandia'){
         if(!haySandia){
             precioSandia=3;
             let parrafo = document.createElement('p');
@@ -103,11 +396,11 @@ carro.addEventListener('drop', e=>{
 
                 document.getElementById('costoSandia').innerHTML=`Sandias: ${precioSandia}€`;
                 hayNaranja=true;
-        }
+                }
 
         
     }
-    if(data=='https://github.com/juanakan/Supermercado/blob/master/images/pepino.png'){
+    if(frutaClass=='pepino'){
         if(!hayPepino){
             precioPepino=3.5;
             let parrafo = document.createElement('p');
@@ -126,7 +419,7 @@ carro.addEventListener('drop', e=>{
 
         
     }
-    if(data=='https://github.com/juanakan/Supermercado/blob/master/images/pimiento.png'){
+    if(frutaClass=='pimiento'){
         if(!hayPimiento){
             precioPimiento=1;
             let parrafo = document.createElement('p');
@@ -141,11 +434,11 @@ carro.addEventListener('drop', e=>{
 
                 document.getElementById('costoPimiento').innerHTML=`Pimientos: ${precioPimiento}€`;
                 hayPimiento=true;
-        }
+                }
 
         
     }
-    if(data=='https://github.com/juanakan/Supermercado/blob/master/images/cebolla.png'){
+    if(frutaClass=='cebolla'){
         if(!hayCebolla){
             precioCebolla=1.5;
             let parrafo = document.createElement('p');
@@ -158,13 +451,13 @@ carro.addEventListener('drop', e=>{
             }else{
                 precioCebolla +=1.5;
 
-            document.getElementById('costoCebolla').innerHTML=`Cebollas: ${precioCebolla}€`;
-            hayCebolla=true;
-        }
+                document.getElementById('costoCebolla').innerHTML=`Cebollas: ${precioCebolla}€`;
+                hayCebolla=true;
+                }
 
         
     }
-    if(data=='https://github.com/juanakan/Supermercado/blob/master/images/platano.png'){
+    if(frutaClass=='platano'){
         if(!hayPlatano){
             precioPlatano = 4;
             let parrafo = document.createElement('p');
@@ -179,10 +472,11 @@ carro.addEventListener('drop', e=>{
 
                 document.getElementById('costoPlatano').innerHTML=`Platanos: ${precioPlatano}€`;
                 hayPlatano=true;
-        }
+                }
 
         
     }
+    //creamos la suma total y lo introducimos en la etiqueta correspondiente con innerHtml
     let precioTotal=precioPera+precioNaranja+precioZanahoria+precioSandia+
         precioPepino+precioPimiento+precioCebolla+precioPlatano;
     document.getElementById('total').innerHTML=`Total: ${precioTotal}€`;
